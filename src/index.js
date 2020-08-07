ac3313838c5860f0e43b7d9b1c4ff2b9c59803fc
thetinyspark <contact@the-tiny-spark.com>
2020-08-07 12:16:17 +0200

    fix import

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './reducers/rootReducer';
+import { Router } from 'react-router-dom';
 
 const store = createStore(rootReducer);
 ReactDOM.render(
